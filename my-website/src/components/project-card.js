9bcc7467df4de66b5aec0897dcd8e75de7663738
deadbird11 <eddiejr@hatfieldfamily.com>
2020-05-22 15:33:32 -0400

    begain building project cards

---
+++ my-website/src/components/project-card.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+import "../components/project-card.css"
+
+class ProjectCard extends React.Component {
+    render() {
+        return (
+            <div class="card">
+                <img src="../../img/going_dynamic-SO.png" alt="" width="500" height="500"></img>
+                <h3>{this.props.title}</h3>
+                <div style={{
+                    textOverflow: `ellipses`,
+                    overflow: `hidden`
+                }}>{this.props.info}</div>
+            </div>
+        )
+    }
+}
+
+export default ProjectCard
