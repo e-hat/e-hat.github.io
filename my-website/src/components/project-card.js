c8ea5c2054ec76ae9e84d92fd51e21b8bef5bda6
deadbird11 <eddiejr@hatfieldfamily.com>
2020-06-15 23:45:14 -0400

    finalized project cards

--- my-website/src/components/project-card.js
+++ my-website/src/components/project-card.js
@@ -6,8 +6,10 @@ class ProjectCard extends React.Component {
     render() {
         return (
             <div className="card">
+                <a href={this.props.href}>
                 <div className="overlay"></div>
-                <h2 className="card-title">{this.props.title}</h2>
+                </a>
+                <h2 className="card-title"><code>{this.props.title}</code></h2>
                 <div className="card-content">
                     <div className="background-circle"></div>
                     <div className="photo">{this.props.img}</div>
