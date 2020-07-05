67f66b75d016ebcb3cf7f2e8ce4f83a237653ec3
deadbird11 <eddiejr@hatfieldfamily.com>
2020-07-05 16:18:29 -0400

    showing picture now

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,39 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Img from "gatsby-image"
+
 import Layout from "../components/layout"
+import Profile from "../components/profile"
+
 import "./index.css"
 
 class IndexPage extends React.Component {
     render() {
+      console.log(this.props);
         return (
+          <Layout>
             <section>
-                <h1>Hello</h1>
-                <article>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </article>
+                <Profile img={
+                  <Img fixed={this.props.data.file.childImageSharp.fixed} />
+                }>
+                  <React.Fragment>Hello World</React.Fragment></Profile>
             </section>
+            </Layout>
         )
     }
 }
 
-export default () => <Layout><IndexPage /></Layout>
+
+export default IndexPage
+
+export const query = graphql`
+query {
+    file(sourceInstanceName: {eq: "static"}, name: {eq: "tldr_card"}) {
+        childImageSharp {
+          fixed(width:200,height:200) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+}
+`
