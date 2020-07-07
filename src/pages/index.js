b984b54260c05f0b553cdcc16813c12465c1b354
deadbird11 <eddiejr@hatfieldfamily.com>
2020-07-07 09:27:12 -0400

    working on profile page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { graphql } from "gatsby"
 import Img from "gatsby-image"
 
 import Layout from "../components/layout"
-import Profile from "../components/profile"
+//import Profile from "../components/profile"
 
 import "./index.css"
 
@@ -13,12 +13,21 @@ class IndexPage extends React.Component {
         return (
           <Layout>
             <section>
-                <Profile img={
-                  <Img fixed={this.props.data.file.childImageSharp.fixed} />
-                }>
-                  <React.Fragment>Hello World</React.Fragment></Profile>
+                <p className="profile-title">Welcome</p>
+                <article className="profile">
+                  <figure>
+                    <Img style={{
+                      borderRadius: `100px`,
+                      float: `right`,
+                      border: `2px solid white`,
+                      }} 
+                      fixed={this.props.data.file.childImageSharp.fixed} 
+                    />
+                  </figure>
+                  My name is Eddie Hatfield. I'm a sophomore at Tufts University who loves playing piano, tennis, doing and learning math, and programming. More specifically, I'm interested in game engine design, graphics, machine learning, and a couple other topics.
+                </article>
             </section>
-            </Layout>
+          </Layout>
         )
     }
 }
