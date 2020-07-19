456a81996c553362196dd9e93d917e147a4137a9
deadbird11 <eddiejr@hatfieldfamily.com>
2020-07-19 18:35:18 -0400

    sending email through the form works now, FINALLY READY FOR CONTENT

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,12 +4,23 @@ import Layout from "../components/layout"
 import "./contact.css"
 
 class ContactPage extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.submitForm = this.submitForm.bind(this);
+        this.state = {
+          status: ""
+        };
+    }
+
     render() {
+        const { status } = this.state;
         return (
             <Layout>
                 <section className="form-content">
                     <h1 className="contact-title">Send me an email</h1>
-                    <form method="post" action="#">
+                    <form method="POST" action="https://formspree.io/xknqpdgd"
+                          onSubmit={this.submitForm}>
                         <label>
                             <div className="input-label">Name</div>
                         <input type="text" name="name" id="name" className="form-input"/>
@@ -17,7 +28,7 @@ class ContactPage extends React.Component {
                         <br />
                         <label>
                             <div className="input-label">Email</div>
-                        <input type="email" name="email" id="email" className="form-input"/>
+                        <input type="email" name="_replyto" id="email" className="form-input"/>
                         </label>
                         <br />
                         <label>
@@ -31,13 +42,35 @@ class ContactPage extends React.Component {
                         </label>
                         <br />
                         <div className="button-container">
-                            <button type="submit" id="submit" className="submit form-button">Send</button>
-                            <input type="reset" value="Clear" className="form-button"/>
+                            {status === "SUCCESS" ? <p className="result-msg">Thanks!</p> : 
+                                                    <><button type="submit" name="submit" className="form-button submit">Send</button>
+                                                    <input type="reset" value="Clear" className="form-button"/></>}
+                            {status === "ERROR" && <p className="result-msg">Ooops! There was an error.</p>}
+                            
                         </div>
                     </form>
                 </section>
             </Layout>
         )
+    }
+
+    submitForm(ev) {
+        ev.preventDefault();
+        const form = ev.target;
+        const data = new FormData(form);
+        const xhr = new XMLHttpRequest();
+        xhr.open(form.method, form.action);
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.onreadystatechange = () => {
+          if (xhr.readyState !== XMLHttpRequest.DONE) return;
+          if (xhr.status === 200) {
+            form.reset();
+            this.setState({ status: "SUCCESS" });
+          } else {
+            this.setState({ status: "ERROR" });
+          }
+        };
+        xhr.send(data);
     }
 }
 
