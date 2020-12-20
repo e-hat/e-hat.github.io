93e11bfe65ab63e6c712955cef1c1b64998d3f3c
deadbird11 <eddiejr@hatfieldfamily.com>
2020-12-19 22:26:55 -0500

    saving work here

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,27 +9,21 @@ import "./index.css"
 
 function IndexPage(props) {
   return (
-    <Layout>
       <section>
-          <p className="profile-title">Welcome</p>
           <article className="profile">
-            <div className="profile-pic">
-              <Img style={{
-                borderRadius: `110px`,
-                border: `2px solid white`,
-                boxShadow: `0px 5px 12px #555555`,
-                margin: `10px`,
-                }} 
-                fixed={props.data.file.childImageSharp.fixed} 
-            />
-            </div>
-          <p className="bio-text">My name is Eddie Hatfield.</p>
-          <p className="bio-text">I'm an undergraduate at Tufts University studying Computer Science and Applied Math.</p>
-          <p className="bio-text">My interests lie in a number of fields, ranging from web development and artificial intelligence to numerical methods and computer graphics.</p>
-          <p className="bio-text">Pretty much, I love math and I love programming, and I especially love when I can do math <i>while</i> I'm programming.</p>
+          <p className="profile-title"><h1>Eddie Hatfield</h1></p>
+          <code className="section-title">about</code>
+          <p className="bio-text">I'm Eddie Hatfield. I'm originally from Cincinnati, Ohio ⚫🟠, but I'm usually in Boston⛄ since I'm an studying CompSci and Applied Math @ Tufts 🐘.  </p>
+          <p className="bio-text">I enjoy playing ping pong🏓, playing video games🕹️, meditating 🧘‍♂️, implementing real-time rendering techniques🖥️, and learning more about all things programming.</p>
+          <code className="section-title">projects</code>
+          <p className="bio-text">
+              Here are some of my favorite projects I've done over the years:
+              <ul>
+                  <li>EFGL</li>
+              </ul>
+          </p>
           </article>
       </section>
-    </Layout>
   );
 }
 
