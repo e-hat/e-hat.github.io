5b31b532b5b036d2e9e2b3b3aee4ebdf35d8a692
Eddie Hatfield <edward.hatfield@tufts.edu>
2021-04-26 20:30:54 -0400

    some updates

--- html/scripts/title_load.js
+++ html/scripts/title_load.js
@@ -1,5 +1,6 @@
 const title = 'eddie hatfield';
 const delay = 200;
+const deviation = 100;
 
 window.onload = () => {
     titleElem = document.getElementById('title');
@@ -9,7 +10,8 @@ window.onload = () => {
     }
 
     for (i = 0; i <= title.length; ++i) {
-        setTimeout(setTitleAtStep(i), delay * i)
+        setTimeout(setTitleAtStep(i), 
+            delay * (i - 1) + Math.random() * deviation - deviation / 2);
     }
 
 }
