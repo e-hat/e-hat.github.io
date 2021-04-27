f0c7a9c43a5f1eb0b4424f49a69384c316798d2a
Eddie Hatfield <edward.hatfield@tufts.edu>
2021-04-26 21:04:50 -0400

    got space to work correctly

--- scripts/title_load.js
+++ scripts/title_load.js
@@ -6,7 +6,12 @@ window.onload = () => {
     titleElem = document.getElementById('title');
 
     const setTitleAtStep = (step) => () => {
-        titleElem.innerHTML = title.substring(0, step + 1);
+        if (title[step] === ' ') {
+            titleElem.innerHTML = title.substring(0, step) + '&nbsp';
+        }
+        else {
+            titleElem.innerHTML = title.substring(0, step + 1);
+        }
     }
 
     for (i = 0; i <= title.length; ++i) {
